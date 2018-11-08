43c8770779bb0a54325fb5a1c1697d37da3fa0e4
SOFIAVM <sofiamaragaukomba@gmail.com>
2018-11-08 07:33:48 +0100

    Update comparador_anyos.py

--- comparador_anyos.py
+++ comparador_anyos.py
@@ -1,19 +1,19 @@
 # coding: utf​-8
 #Autora: Sofía 
 #Fecha creación: 07/11/18
-#Ejercicio: comparador-años.py
+#Ejercicio: comparador_años.py
 #Escriba un programa que pida el año actual y un año cualquiera y que escriba cuántos
 #años han pasado desde ese año o cuántos años faltan para llegar a ese año.
 #COMPARADOR DE AÑOS:
-#¡En qué año estamos?: 2018
+#¿En qué año estamos?: 2018
 #Escriba un año cualquiera: 2020
-#Para llegar al año 2020 faltan 2 años
+#Para llegar al año 2020 faltan 2 años.
 #COMPARADOR DE AÑOS:
-#¡En qué año estamos?: 2018
+#¿En qué año estamos?: 2018
 #Escriba un año cualquiera: 1997
-#Desde el año 1997 han pasado 21 años
+#Desde el año 1997 han pasado 21 años.
 #COMPARADOR DE AÑOS:
-#¡En qué año estamos?: 2018
+#¿En qué año estamos?: 2018
 #Escriba un año cualquiera: 2018
 #¡Son el mismo año!
 
