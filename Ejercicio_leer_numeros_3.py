dcb7a84ac68986609dddb3125b4e103b4b3aa983
SOFIAVM <sofiamaragaukomba@gmail.com>
2018-11-19 00:51:02 +0100

    Jueves 15-11-18

---
+++ Ejercicio_leer_numeros_3.py
@@ -0,0 +1,18 @@
+#coding:utf-8
+#Autora: Sofía
+#Fecha creación: 15/11/18
+#Ejercicio_leer_numeros_3.py
+"""EJERCICIO 1
+Leer 2 números y mostrar el mayor
+*****************************************************************"""
+print("Leer 2 números y mostrar el mayor")
+num1 = 0
+num2 = 0
+num1 = int(input("Escribe un número: "))
+num2 = int(input("Escribe un número: "))
+
+
+if num1 > num2 :
+        print("El mayor el número uno:  " + str(num1))
+else:
+       print("Es mayor el número dos:  " + str(num2))
