ef0bd47aed0bd80f73b926e3dda759c531b8eee8
SOFIAVM <sofiamaragaukomba@gmail.com>
2018-11-19 00:49:14 +0100

    Jueves  8-11-18

---
+++ Ejercicio_leer_numeros_1.py
@@ -0,0 +1,19 @@
+#coding:utf-8
+#Autora: Sofía
+#Fecha creación: 08/11/18
+#Ejercicio_leer_numeros_1.py
+""" Progama que llegeixi un nombre enter i digui si és pisitiu o no.
+JOC DE PROVES
+Entrada          Sortida
+    3              'positiu'
+   -2              'no positiu'
+    0               'positiu'"""
+
+numero = int(input("Escriu un nombre: "))
+
+if (numero >= 0):
+    print("És positiu")
+    
+else:
+    print("És negatiu")
+
---
+++ Ejercicio_leer_numeros_2.py
@@ -0,0 +1,16 @@
+#coding:utf-8
+#Autora: Sofía
+#Fecha creación: 08/11/18
+#Ejercicio_leer_numeros_2.py
+"""LLegir el programa i treure el seu joc de proves:
+nombre = int(input())
+if (nombre > 0):
+    print("És positiu")
+if (nombre < 0):
+    print("És negatiu")"""
+
+JOC DE PROVES
+    Entrada        Sortida
+     20            'positiu'
+     -2            'negatiu'
+      0             res
